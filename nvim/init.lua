c81eb51bd45f3d5e18a43cd776941089c0678c6a
hms <hms@hayden.computer>
2022-10-16 03:21:53 +0000

    nvim is basically just gruvbox

--- nvim/init.lua
+++ nvim/init.lua
@@ -8,9 +8,3 @@ require "settings/styles"
 -- all configurations for plugins
 ----------------------------------
 require "plugins"
-require "plugins/configs/cmp"
-require "plugins/configs/telescope"
--------------------------------------------
--- mason --> mason-lspconfig --> lspconfig 
--- must be setup in this order
--------------------------------------------
